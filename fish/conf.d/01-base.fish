351f364a2b352c6be77ccb844a069e74d5e48f32
th4s <th4s@metavoid.xyz>
2024-01-22 18:26:47 +0100

    Suppress ssh-agent output

--- fish/conf.d/01-base.fish
+++ fish/conf.d/01-base.fish
@@ -14,7 +14,7 @@ set -g fish_greeting
 
 # Set up ssh agent
 if test -z (pgrep ssh-agent | string collect)
-    eval (ssh-agent -c) 
+    eval (ssh-agent -c) > /dev/null
     set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
     set -Ux SSH_AGENT_PID $SSH_AGENT_PID
 end
