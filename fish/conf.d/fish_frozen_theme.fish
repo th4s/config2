5df8df2dfe158c8a1bfe186b9b441eaa1bfcd1ed
th4s <th4s@metavoid.xyz>
2026-01-07 19:27:20 +0100

    migrate key bindings

---
+++ fish/conf.d/fish_frozen_theme.fish
@@ -0,0 +1,37 @@
+# This file was created by fish when upgrading to version 4.3, to migrate
+# theme variables from universal to global scope.
+# Don't edit this file, as it will be written by the web-config tool (`fish_config`).
+# To customize your theme, delete this file and see
+#     help interactive#syntax-highlighting
+# or
+#     man fish-interactive | less +/^SYNTAX.HIGHLIGHTING
+# for appropriate commands to add to ~/.config/fish/config.fish instead.
+# See also the release notes for fish 4.3.0 (run `help relnotes`).
+
+set --global fish_color_autosuggestion 555 brblack
+set --global fish_color_cancel -r
+set --global fish_color_command F92672
+set --global fish_color_comment 75715E
+set --global fish_color_cwd 66D9EF
+set --global fish_color_cwd_root red
+set --global fish_color_end F8F8F2
+set --global fish_color_error F8F8F2 --background=F92672
+set --global fish_color_escape 66D9EF
+set --global fish_color_history_current --bold
+set --global fish_color_host normal
+set --global fish_color_host_remote yellow
+set --global fish_color_normal F8F8F2
+set --global fish_color_operator AE81FF
+set --global fish_color_param A6E22E
+set --global fish_color_quote E6DB74
+set --global fish_color_redirection AE81FF
+set --global fish_color_search_match --background=49483E
+set --global fish_color_selection white --bold --background=brblack
+set --global fish_color_status red
+set --global fish_color_user brgreen
+set --global fish_color_valid_path --underline
+set --global fish_pager_color_completion 75715E
+set --global fish_pager_color_description 49483E
+set --global fish_pager_color_prefix F8F8F2
+set --global fish_pager_color_progress F8F8F2
+set --global fish_pager_color_selected_background -r
